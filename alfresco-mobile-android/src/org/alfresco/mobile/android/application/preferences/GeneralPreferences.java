07383e4ba23193f666dcf4b70ea1ebad399cd6ce
Paul Kolomiets <paul.kolomiets@gmail.com>
2014-06-19 08:02:23 +0400

    disabled autosync setting once more
    This reverts commit a4b32e8e67013463b08b0dad50bd821980ff12a3.

--- alfresco-mobile-android/src/org/alfresco/mobile/android/application/preferences/GeneralPreferences.java
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/preferences/GeneralPreferences.java
@@ -325,6 +325,11 @@ public class GeneralPreferences extends PreferenceFragment
         Preference odsSyncPref = findPreference(ODS_SYNCHONISATION_BUTTON);
         refreshOdsSync();
 
+        if (odsSyncPref == null)
+        {
+            return;
+        }
+
         odsSyncPref.setOnPreferenceClickListener(new OnPreferenceClickListener()
         {
             @Override
@@ -386,6 +391,12 @@ public class GeneralPreferences extends PreferenceFragment
     {
         final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
         Preference odsSyncPref = findPreference(ODS_SYNCHONISATION_BUTTON);
+
+        if (odsSyncPref == null)
+        {
+            return;
+        }
+
         String id = sharedPref.getString(ODS_SYNCHONISATION, "");
         odsSyncPref.setSummary((id != null && !"".equals(id)) ? R.string.settings_autosync_settings_on
                 : R.string.settings_autosync_settings_off);
