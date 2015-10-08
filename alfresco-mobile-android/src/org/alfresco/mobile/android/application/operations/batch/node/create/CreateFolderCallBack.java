20eeececa62bed8411a8e40ceab732cc70e0dc73
Paul Kolomiets <paul.kolomiets@gmail.com>
2015-10-08 13:07:36 +0300

    show error message for creating folder with name already in use

--- alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/node/create/CreateFolderCallBack.java
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/node/create/CreateFolderCallBack.java
@@ -1,27 +1,33 @@
 /*******************************************************************************
  * Copyright (C) 2005-2013 Alfresco Software Limited.
- *  
- *  This file is part of Alfresco Mobile for Android.
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0
- *  
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * <p/>
+ * This file is part of Alfresco Mobile for Android.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 package org.alfresco.mobile.android.application.operations.batch.node.create;
 
-import org.alfresco.mobile.android.api.model.Folder;
-import org.opendataspace.android.app.R;
-import org.alfresco.mobile.android.application.operations.batch.impl.AbstractBatchOperationCallback;
-
 import android.content.Context;
+import android.os.Bundle;
+
+import org.alfresco.mobile.android.api.exceptions.AlfrescoServiceException;
+import org.alfresco.mobile.android.api.model.Folder;
+import org.alfresco.mobile.android.application.commons.fragments.SimpleAlertDialogFragment;
+import org.alfresco.mobile.android.application.manager.ActionManager;
+import org.alfresco.mobile.android.application.operations.Operation;
+import org.alfresco.mobile.android.application.operations.batch.impl.AbstractBatchOperationCallback;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisNameConstraintViolationException;
+import org.opendataspace.android.app.R;
 
 public class CreateFolderCallBack extends AbstractBatchOperationCallback<Folder>
 {
@@ -33,4 +39,21 @@ public class CreateFolderCallBack extends AbstractBatchOperationCallback<Folder>
         complete = getBaseContext().getString(R.string.create_folder_complete);
     }
 
+    @Override
+    public void onError(Operation<Folder> task, Exception e)
+    {
+        super.onError(task, e);
+
+        if (e instanceof AlfrescoServiceException && e.getCause() instanceof CmisNameConstraintViolationException)
+        {
+            Bundle b = new Bundle();
+            b.putInt(SimpleAlertDialogFragment.PARAM_ICON, R.drawable.ic_edit);
+            b.putInt(SimpleAlertDialogFragment.PARAM_TITLE, R.string.error_duplicate_title);
+            b.putString(SimpleAlertDialogFragment.PARAM_MESSAGE_STRING,
+                    String.format(context.getString(R.string.error_duplicate_rename),
+                            ((CreateFolderThread) task).getFolderName()));
+            b.putInt(SimpleAlertDialogFragment.PARAM_POSITIVE_BUTTON, android.R.string.ok);
+            ActionManager.actionDisplayDialog(context, b);
+        }
+    }
 }
