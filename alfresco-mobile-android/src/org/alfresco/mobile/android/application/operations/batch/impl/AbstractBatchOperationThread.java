337a4f0c44a1fc958b43c6185b3a43405c12c799
Paul Kolomiets <paul.kolomiets@gmail.com>
2015-10-21 12:08:04 +0300

    make operation completed broadcast to be sent after operation status update

--- alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/impl/AbstractBatchOperationThread.java
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/impl/AbstractBatchOperationThread.java
@@ -1,21 +1,23 @@
 /*******************************************************************************
  * Copyright (C) 2005-2013 Alfresco Software Limited.
- * 
- *  This file is part of Alfresco Mobile for Android.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *  http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * <p/>
+ * This file is part of Alfresco Mobile for Android.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 package org.alfresco.mobile.android.application.operations.batch.impl;
+
+import android.content.Context;
 
 import org.alfresco.mobile.android.api.asynchronous.LoaderResult;
 import org.alfresco.mobile.android.application.accounts.Account;
@@ -25,8 +27,6 @@ import org.alfresco.mobile.android.application.operations.OperationRequest;
 import org.alfresco.mobile.android.application.operations.batch.BatchOperationManager;
 import org.alfresco.mobile.android.application.operations.impl.AbstractOperationThread;
 import org.opendataspace.android.ui.logging.OdsLog;
-
-import android.content.Context;
 
 public abstract class AbstractBatchOperationThread<T> extends AbstractOperationThread<T>
 {
@@ -76,13 +76,14 @@ public abstract class AbstractBatchOperationThread<T> extends AbstractOperationT
                     resultStatus = STATUS_CANCEL;
                 }
             }
-            else
-            {
-                listener.onPostExecute(this, result.getData());
-            }
         }
         saveStatus(resultStatus);
         BatchOperationManager.getInstance(context).notifyCompletion(getOperationId(), resultStatus);
+
+        if (listener != null && !result.hasException())
+        {
+            listener.onPostExecute(this, result.getData());
+        }
     }
 
     protected void onCancelled(LoaderResult<T> result)
