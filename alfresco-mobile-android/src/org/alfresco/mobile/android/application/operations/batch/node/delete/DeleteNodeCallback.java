9c2bb29d691b66ad0aef499c47ab7c2fc10093fb
Paul Kolomiets <paul.kolomiets@gmail.com>
2016-05-12 13:17:50 +0300

    remove sucess toast if delete operation failed

--- alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/node/delete/DeleteNodeCallback.java
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/node/delete/DeleteNodeCallback.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (C) 2005-2013 Alfresco Software Limited.
- * 
+ * <p>
  * This file is part of Alfresco Mobile for Android.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,13 +17,14 @@
  ******************************************************************************/
 package org.alfresco.mobile.android.application.operations.batch.node.delete;
 
-import org.opendataspace.android.app.R;
-import org.alfresco.mobile.android.application.operations.batch.impl.AbstractBatchOperationCallback;
-
 import android.content.Context;
 
+import org.alfresco.mobile.android.application.operations.OperationRequest;
+import org.alfresco.mobile.android.application.operations.OperationsGroupResult;
+import org.alfresco.mobile.android.application.operations.batch.impl.AbstractBatchOperationCallback;
+import org.opendataspace.android.app.R;
+
 /**
- * 
  * @author Jean Marie Pascal
  */
 public class DeleteNodeCallback extends AbstractBatchOperationCallback<Void>
@@ -33,5 +34,17 @@ public class DeleteNodeCallback extends AbstractBatchOperationCallback<Void>
         super(context, totalItems, pendingItems);
         inProgress = getBaseContext().getString(R.string.delete_in_progress);
         complete = getBaseContext().getString(R.string.delete_complete);
+    }
+
+    @Override
+    public void onPostExecution(OperationsGroupResult result)
+    {
+        if (result.notificationVisibility != OperationRequest.VISIBILITY_HIDDEN &&
+                (result.completeRequest == null || result.completeRequest.isEmpty()))
+        {
+            return;
+        }
+
+        super.onPostExecution(result);
     }
 }
--- alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/node/delete/DeleteNodeThread.java
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/operations/batch/node/delete/DeleteNodeThread.java
@@ -1,31 +1,31 @@
 /*******************************************************************************
  * Copyright (C) 2005-2013 Alfresco Software Limited.
- * 
- *  This file is part of Alfresco Mobile for Android.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *  http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * <p>
+ * This file is part of Alfresco Mobile for Android.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 package org.alfresco.mobile.android.application.operations.batch.node.delete;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 
 import org.alfresco.mobile.android.api.asynchronous.LoaderResult;
 import org.alfresco.mobile.android.application.intent.IntentIntegrator;
 import org.alfresco.mobile.android.application.operations.batch.impl.AbstractBatchOperationRequestImpl;
 import org.alfresco.mobile.android.application.operations.batch.node.NodeOperationThread;
 import org.opendataspace.android.ui.logging.OdsLog;
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 
 public class DeleteNodeThread extends NodeOperationThread<Void>
 {
@@ -39,12 +39,16 @@ public class DeleteNodeThread extends NodeOperationThread<Void>
     @Override
     public LoaderResult<Void> doInBackground()
     {
-        LoaderResult<Void> result  = new LoaderResult<Void>();
+        LoaderResult<Void> result = new LoaderResult<Void>();
+
         try
         {
             result = super.doInBackground();
 
-            if (parentFolder == null) { return result; }
+            if (parentFolder == null)
+            {
+                return result;
+            }
 
             session.getServiceRegistry().getDocumentFolderService().deleteNode(node);
         }
@@ -66,5 +70,4 @@ public class DeleteNodeThread extends NodeOperationThread<Void>
         broadcastIntent.putExtra(IntentIntegrator.EXTRA_DATA, b);
         return broadcastIntent;
     }
-
 }
